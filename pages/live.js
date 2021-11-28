f60244a320b867fbefb9bd17b3729fec20dd2761
unknown <michaelmedewase@gmail.com>
2021-11-28 09:48:21 +0100

    added live

--- pages/live.js
+++ pages/live.js
@@ -6,9 +6,12 @@ import AuthLayout from '../components/layout/auth'
 const Index = ()=>{
 
     return(
-       <AuthLayout>
-           <iframe width="560" height="315" src="http://172.105.77.49:5080/LiveStream/play.html?name=615634133307500451422115" frameborder="0" allowfullscreen></iframe>
-        </AuthLayout>
+        <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+            <iframe width="560" height="315" src="http://172.105.77.49:5080/LiveStream/play.html?name=615634133307500451422115" frameborder="0" allowfullscreen></iframe>
+        </div>
+   
+          
+        
     )
 }
 
